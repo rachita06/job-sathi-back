9aeb79d0dce0c5846f4d966caaa2eb7b3ae6bdc9
rachita06 <rachitaadhikari06@gmail.com>
2026-01-31 23:13:26 +0545

    initial resume project setup

---
+++ src/main/java/com/example/jobsathi/controller/GreetingController.java
@@ -0,0 +1,31 @@
+package com.example.jobsathi.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String getGreeting() {
+        return "This is resume analyzer powered by AI";
+    }
+
+    @GetMapping("/get-time")
+    public LocalTime getTime(){
+        return  LocalTime.now();
+    }
+
+    @GetMapping("/get-date")
+    public LocalDate getDate(){
+        return LocalDate.now();
+    }
+
+}
---
+++ src/test/java/com/example/jobsathi/JobSathiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jobsathi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSathiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
